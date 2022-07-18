0c2d71ec37002ac1a4fe0e912df7b47ed6a457e9
dmajorky <majordecorise@yahoo.com>
2022-07-17 22:09:15 -0400

    Third commit

--- index.py
+++ index.py
@@ -12,3 +12,20 @@ data.drop([75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,
 data.drop(['Rk','Lg','Age','Ht','Wt','G','MP','eFG%','TOV%','ORB%','FT/FGA','ORtg'],axis=1,inplace=True)
 
 print(data)
+
+# found the average pace in NBA history
+mean_data = data['Pace'].mean()
+
+print(mean_data)
+
+# found lowest pace(min) & highest pace(max) in NBA history
+min_data = data['Pace'].min()
+max_data = data['Pace'].max()
+
+print(min_data)
+print(max_data)
+
+# found average pace of each decade
+seventys_average = data['Pace'].mean('48','47','46','45','44','43')
+
+print(seventys_average)
